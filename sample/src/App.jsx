8becf43d682516af56f9dbaa88fb9f2d4b879bff
satheeshaithagoni <aithagonisg@gmail.com>
2024-06-04 16:59:17 +0530

    added initial component

---
+++ sample/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import React from "react";
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <div>this is first div</div>
+      <div>this is second div</div>
+    </React.Fragment>
+  );
+}
